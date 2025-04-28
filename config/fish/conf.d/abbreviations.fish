8bb756bba22d4eeec2ef288bb2c0f3649e47a7a3
duck-that-quacks <duck-that-quacks@noreply.gitea.com>
2025-04-28 14:23:52 +0500

    Fish: some more abbreviations

--- config/fish/conf.d/abbreviations.fish
+++ config/fish/conf.d/abbreviations.fish
@@ -1,4 +1,30 @@
-abbr -a	nv nvim
+# General
+abbr -a nv nvim
 abbr -a lg lazygit
 abbr -a tx tmux
 abbr -a pc pacman
+abbr -a cl clear
+
+# Git
+abbr -a g git
+abbr -a ga git add
+abbr -a gs git status
+abbr -a gss git status -s
+abbr -a gc git commit
+abbr -a gca git commit --amend
+abbr -a gr git rebase
+abbr -a grc git rebase --continue
+abbr -a gg git push
+abbr -a gp git pull
+abbr -a gw git switch
+abbr -a go git restore
+abbr -a gr git reset
+abbr -a goo git restore -S
+abbr -a gri git rebase -i
+abbr -a ge git remote
+abbr -a ger git remote remove
+abbr -a gea git remote add
+abbr -a ges git remote show -n
+abbr -a gf git fetch
+abbr -a gd git diff
+abbr -a gds git diff --staged
