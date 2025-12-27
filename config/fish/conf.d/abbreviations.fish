799cafa997c12ce81bf15ec53a3dba775f41ec32
duck-that-quacks <duck-that-quacks@noreply.gitea.com>
2025-12-28 02:20:40 +0500

    Fish: new zig abbreviations

--- config/fish/conf.d/abbreviations.fish
+++ config/fish/conf.d/abbreviations.fish
@@ -10,31 +10,36 @@ end
 abbr -a dotdot -r "^\.\.+\$" -f fastparentcd
 
 # Git
-abbr -a g git
 abbr -a ga git add
-abbr -a gs git status
-abbr -a gss git status -s
-abbr -a gc git commit
-abbr -a gca git commit --amend
-abbr -a gb git rebase
 abbr -a gbc git rebase --continue
-abbr -a gg git push
-abbr -a gp git pull
-abbr -a gw git switch
-abbr -a go git restore
-abbr -a gr git reset
-abbr -a goo git restore -S
-abbr -a gri git rebase -i
-abbr -a ge git remote
-abbr -a ger git remote remove
-abbr -a gea git remote add
-abbr -a ges git remote show -n
-abbr -a gf git fetch
+abbr -a gb git rebase
+abbr -a gca git commit --amend
+abbr -a gc git commit
 abbr -a gd git diff
 abbr -a gds git diff --staged
+abbr -a gea git remote add
+abbr -a ge git remote
+abbr -a ger git remote remove
+abbr -a ges git remote show -n
+abbr -a gf git fetch
+abbr -a gg git push
+abbr -a g git
+abbr -a go git restore
+abbr -a goo git restore -S
+abbr -a gp git pull
+abbr -a gr git reset
+abbr -a gri git rebase -i
+abbr -a gs git status
+abbr -a gss git status -s
+abbr -a gw git switch
 
 # Zig
-abbr -a zb zig build
+abbr -a zbf zig build -OReleaseFast
+abbr -a zbrf zig build run -OReleaseFast
 abbr -a zbr zig build run
-abbr -a zbe zig build-exe
 abbr -a zbt zig build test
+abbr -a zb zig build
+abbr -a zrf zig run -OReleaseFast
+abbr -a zr zig run
+abbr -a zt zig test
+abbr -a zx zig build-exe
